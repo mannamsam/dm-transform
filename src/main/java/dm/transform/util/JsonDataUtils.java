40e988b50e5cc26dd70a9f292cff6a40da42e276
RAM MANNAM <mannam@RAMs-MacBook-Pro.local>
2018-03-21 11:02:08 -0400

    JsonUtils Wrapper

---
+++ src/main/java/dm/transform/util/JsonDataUtils.java
@@ -0,0 +1,12 @@
+package dm.transform.util;
+
+import com.bazaarvoice.jolt.JsonUtils;
+
+/**
+ * 
+ * @author mannam
+ *
+ */
+public class JsonDataUtils extends JsonUtils {
+	//implement any custom related to Data Management
+}
